b9cdd6d5d66fb5ba2e10adb0b9c4b25f5afc110b
M4thG33k <pepethemathgeek@gmail.com>
2016-06-27 10:50:22 -0500

    Fixes crash with Furnace Upgrades

--- src/main/java/com/m4thg33k/lit/items/ItemFurnaceUpgrade.java
+++ src/main/java/com/m4thg33k/lit/items/ItemFurnaceUpgrade.java
@@ -9,6 +9,7 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -34,7 +35,14 @@ public class ItemFurnaceUpgrade extends Item {
 
     @Override
     public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        TileImprovedFurnace te = (TileImprovedFurnace)worldIn.getTileEntity(pos);
+        TileEntity tile = worldIn.getTileEntity(pos);
+
+        if (tile == null || !(tile instanceof TileImprovedFurnace))
+        {
+            return EnumActionResult.PASS;
+        }
+
+        TileImprovedFurnace te = (TileImprovedFurnace)tile;
 
         if (te.canReceiveUpgrade())
         {
